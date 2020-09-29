986c90d566f40871bf4a1b4cf5e8e82b72f3384c
Scot Morse <morses@wou.edu>
2020-09-29 13:41:33 -0700

    Add files for in-class exercise 1 for week 1 on regular expressions

---
+++ Ex1/problems.js
@@ -0,0 +1,47 @@
+// Read https://javascript.info/regular-expressions then do these
+
+/* Here's where to solve the problem.  Write your code in the appropriate function below. 
+   Return true to indicate success, false otherwise. Print out to the console as directed in 
+   the problem statement.
+
+   For problems 1 and 2 there are testing files available.  Paste one of these into the text field for
+   the respective problem:
+
+   Problem 1: https://wou-cs46x-resources.netlify.app/data/a1p1.json
+   Problem 2: https://wou-cs46x-resources.netlify.app/data/a1p2.json
+*/
+function p1(input)
+{
+    console.log(`Running validation for problem p1 for input: "${input}"`);
+    return false;
+}
+
+function p2(input)
+{
+    return false;
+}
+
+/*
+    To test this input you'll have to replace all newlines with \n   I'll show you how to do this in VS Code.
+*/
+function p3(input)
+{
+    return false;
+}
+/*
+    You'll also need to replace newlines in this problem.
+*/
+function p4(input)
+{
+    return false;
+}
+
+function p5(input)
+{
+    return false;
+}
+
+function p6(input)
+{
+    return false;
+}
